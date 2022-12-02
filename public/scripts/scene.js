7b9142f88d239d8b5133b61a27336f2bdd065cf5
wwwazz <wwwazz333@gmail.com>
2022-12-02 04:51:17 +0100

    question scen

--- public/scripts/scene.js
+++ public/scripts/scene.js
@@ -1,19 +1,28 @@
 
 import { FontLoader, TextGeometry } from 'https://cdnjs.cloudflare.com/ajax/libs/three.js/r128/three.module.js';
 
-function startGame() {
+async function startGame() {
+	var dataQuestion;
+	var currQuestion = 0;
+	var jo = await fetch("reponse/reponse.json");
+	if (jo.ok) {
+		dataQuestion = await jo.json();
+	}
+
+
 	const scene = new THREE.Scene();
-	const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+	const camera = new THREE.PerspectiveCamera(90, window.innerWidth / window.innerHeight, 0.1, 1000);
 
 	const renderer = new THREE.WebGLRenderer();
 	renderer.setSize(window.innerWidth, window.innerHeight);
 	document.body.appendChild(renderer.domElement);
 
 
-	camera.position.z = -10;
+	camera.position.z = -8;
 	camera.position.y = 2;
 	camera.lookAt(scene.position);
 	camera.position.y += 1;
+
 
 
 	var lastScene = scene;
@@ -33,6 +42,7 @@ function startGame() {
 
 
 			render(newScene);
+			reloadText()
 		}
 	}
 
@@ -85,7 +95,33 @@ function startGame() {
 	}
 	function clickObj() {
 		if (currentPorte != null) {
-			console.log(currentPorte.object.geometry.name);
+			var rep;
+			switch (currentPorte.object.geometry.name) {
+				case "p_bleue":
+					rep = dataQuestion[currQuestion].gauche.rep;
+					break;
+				case "p_verte":
+					rep = dataQuestion[currQuestion].millieu.rep;
+					break;
+				case "p_rouge":
+					rep = dataQuestion[currQuestion].droite.rep;
+					break;
+
+				default:
+					break;
+			}
+			console.log(rep);
+			if (rep) {
+				currQuestion++;
+
+				let d = dataQuestion[currQuestion];
+
+				setTexts(d.question, d.gauche.txt, d.millieu.txt, d.droite.txt);
+				render(lastScene)
+			}else{
+				alert("Faux!");
+			}
+
 		}
 	}
 
@@ -96,22 +132,96 @@ function startGame() {
 	// 	const geometry = new THREE.TextGeometry(text);
 	// });f
 
-
+	const PI = 3.1415;
 	const loaderFont = new FontLoader();
-	const text = "Hello World"
-	loaderFont.load('/font/roboto_regular.json', function (font) {
-		const geometry = new TextGeometry(text, {
-			font: font,
-			size: 80,
-			height: 5,
-			curveSegments: 12,
-			bevelEnabled: true,
-			bevelThickness: 10,
-			bevelSize: 8,
-			bevelOffset: 0,
-			bevelSegments: 5
+	var texts = [];
+
+
+
+
+	function setTexts(question, gauche, milleu, droite) {
+		for (const txt of texts) {
+			lastScene.remove(txt);
+		}
+
+		addText(milleu, -2.7, 5, 0, PI, false);//milleiu
+		addText(gauche, 2.5, 4, -3, PI / 4 + PI, false);//gauche
+		addText(droite, -3, 4, -6, 2.55 * PI, false);//sdroite
+
+		addText(question, -1, 1, -4, PI, true);//questoin
+	}
+
+	let d = dataQuestion[currQuestion];
+
+	setTexts(d.question, d.gauche.txt, d.millieu.txt, d.droite.txt);
+
+
+
+	function getCenterPoint(mesh) {
+		var geometry = mesh.geometry;
+		geometry.computeBoundingBox();
+		var center = new THREE.Vector3();
+		geometry.boundingBox.getCenter(center);
+		mesh.localToWorld(center);
+		return center;
+	}
+
+	function addText(str, x, y, z, rot, question) {
+		loaderFont.load('/font/roboto_regular.json', function (font) {
+			const textGeometry = new TextGeometry(str.replaceAll(" ", "\r\r"), {
+				font: font,
+				size: 0.2,
+				height: 0.001,
+				bevelEnabled: true,
+
+				// curveSegments: 0,
+				// bevelEnabled: true,
+				bevelSize: 0.00001,
+				bevelThickness: 0.01,
+				// bevelSize: 0,
+				// bevelOffset: 0,
+				// bevelSegments: 1
+
+			});
+
+
+			var textMesh = new THREE.Mesh(textGeometry, [
+				new THREE.MeshPhongMaterial({ emissive: "grey" }), new THREE.MeshPhongMaterial({ emissive: "grey" })
+			]);
+			texts.push(textMesh);
+			var center = getCenterPoint(textMesh);
+			textMesh.geometry.applyMatrix(new THREE.Matrix4().makeTranslation(-center.x, -center.y, -center.z));
+
+			if (!question)
+				textMesh.rotation.set(0, rot, 0);
+			else
+				textMesh.rotation.set(PI / 3, rot, 0);
+
+
+			textMesh.geometry.applyMatrix(new THREE.Matrix4().makeTranslation(-center.x, -center.y, -center.z));
+
+			lastScene.add(textMesh);
+			textMesh.position.set(x, y, z); // position text here x, y, z
+
+			console.log("textMesh added to scene");
+			textMesh.castShadow = true; // object can cast shadows (default = false)
+			textMesh.receiveShadow = true; // object can receive shadows (default= false)
+
+			// camera.lookAt(textMesh.position);
+
+			render(lastScene)
 		});
-	});
+	}
+
+	function reloadText() {
+		for (const tt of texts) {
+			lastScene.add(tt);
+		}
+		render(lastScene)
+
+	}
+
+
 
 }
 
